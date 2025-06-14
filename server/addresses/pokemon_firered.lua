6e8231325f13c771a50b97401967b794c50fe435
nauanelinhares <nauane.linhares@gmail.com>
2025-06-14 15:15:19 -0300

    chore: add game addresses like constants

---
+++ server/addresses/pokemon_firered.lua
@@ -0,0 +1,17 @@
+PokemonFirered = {
+
+}
+
+
+
+
+
+PokemonFirered.Addresses = {
+    base_stats_table_address = 0x8254784,
+    pokemon_party_address = 0x2024284,
+    base_stats_entry_size = 28,
+    pokemon_party_entry_size = 100
+}
+
+
+return PokemonFirered
---
+++ server/addresses/pokemon_unbound.lua
@@ -0,0 +1,15 @@
+PokemonUnbound = {
+
+}
+
+
+
+PokemonUnbound.Addresses = {
+    base_stats_table_address = 0x99e0c9c,
+    pokemon_party_address = 0x2024284,
+    base_stats_entry_size = 28,
+    pokemon_party_entry_size = 100
+}
+
+
+return PokemonUnbound
